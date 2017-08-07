7495d3e081371eddc3a5bc131852d6250f729131
VitorSouza23 <vitordesolza@gmail.com>
2017-08-07 16:11:41 -0300

    Correção nas mensagens

--- src/clienteudp/ClienteUDP.java
+++ src/clienteudp/ClienteUDP.java
@@ -22,23 +22,26 @@ public class ClienteUDP {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Scanner entrada = new Scanner(System.in);
+        //Scanner entrada = new Scanner(System.in);
         try {
             //Primeiro argumento é o IP do host destino
-            System.out.println("Digite o ID do Servidor:");
-            InetAddress addr = InetAddress.getByName(entrada.nextLine());
-            System.out.println("Digite a porta do Servidor:");
-            int port = Integer.parseInt(entrada.nextLine());
-            System.out.println("Digite a Mensagem:");
-            byte[] msg = entrada.nextLine().getBytes();
+            //System.out.println("Digite o ID do Servidor:");
+            String id = JOptionPane.showInputDialog(null, "Digite o ID do Servidor:", "ID do Servidor", JOptionPane.INFORMATION_MESSAGE);
+            InetAddress addr = InetAddress.getByName(id);
+            //System.out.println("Digite a porta do Servidor:");
+            int port = Integer.parseInt(JOptionPane.showInputDialog(null, "Digite a Porta do Servidor:", "Porta", JOptionPane.INFORMATION_MESSAGE));
+            //System.out.println("Digite a Mensagem:");
+            byte[] msg = JOptionPane.showInputDialog(null, "Digite a mensagem:", "Mensagem", JOptionPane.INFORMATION_MESSAGE).getBytes();
             //Monta o pacote a ser enviado
             DatagramPacket pkg = new DatagramPacket(msg, msg.length, addr, port);
             // Cria o DatagramSocket que será responsável por enviar a mensagem
             DatagramSocket ds = new DatagramSocket();
             //Envia a mensagem
             ds.send(pkg);
-            System.out.println("Mensagem enviada para: " + addr.getHostAddress() + "\n"
-                    + "Porta: " + port + "\n" + "Mensagem: " + new String(msg));
+            String mensagem = "Mensagem enviada para: " + addr.getHostAddress() + "\n"
+                    + "Porta: " + port + "\n" + "Mensagem: " + new String(msg);
+            JOptionPane.showMessageDialog(null, mensagem, "Sucesso!", JOptionPane.INFORMATION_MESSAGE);
+            System.out.println(mensagem);
 
             //Fecha o DatagramSocket
             ds.close();
